7823f0d6d18be8db757ac8a6553e2cafa6d4ea3c
anushkasingh09 <92538034+anushkasingh09@users.noreply.github.com>
2022-05-28 20:29:22 +0530

    Write a program to Shuffling a List.

---
+++ Shuffle.py
@@ -0,0 +1,16 @@
+import random
+  
+  sample_list = [1, 2, 3, 4, 5]
+  
+print("Original list : ")
+print(sample_list)
+  
+# first shuffle
+random.shuffle(sample_list)
+print("\nAfter the first shuffle : ")
+print(sample_list)
+  
+# second shuffle
+random.shuffle(sample_list)
+print("\nAfter the second shuffle : ")
+print(sample_list)
