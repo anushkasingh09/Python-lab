10e918fa6dbfda416aa587f70929f0071cfc8219
anushkasingh09 <92538034+anushkasingh09@users.noreply.github.com>
2022-05-28 15:48:05 +0530

    Python Program to Merge Two Dictionaries

---
+++ Merge.py
@@ -0,0 +1,9 @@
+first_Dict = {1: 'apple', 2: 'Banana' , 3: 'Orange'}
+second_Dict = { 4: 'Kiwi', 5: 'Mango'}
+print("First Dictionary: ", first_Dict)
+print("Second Dictionary: ", second_Dict)
+
+first_Dict.update(second_Dict)
+    
+print("\nAfter Concatenating : ")
+print(first_Dict)
