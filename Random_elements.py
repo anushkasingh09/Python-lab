6d818e87c2af396ae18738960d684e33eb400bbc
anushkasingh09 <92538034+anushkasingh09@users.noreply.github.com>
2022-05-28 20:27:08 +0530

    Selecting random elements from the list, string, and tuple

---
+++ Random_elements.py
@@ -0,0 +1,8 @@
+import random
+  list1 = [1, 2, 3, 4, 5, 6]
+print(random.choice(list1))
+  
+string = "stack"
+print(random.choice(string))
+  tuple1 = (1, 2, 3, 4, 5)
+print(random.choice(tuple1))
