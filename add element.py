c4ef2c80cae00b7de7199b79fbffc942ade65c1b
anushkasingh09 <92538034+anushkasingh09@users.noreply.github.com>
2022-05-28 15:38:39 +0530

    Python Program to Add Key-Value Pair to a Dictionary

---
+++ add element.py
@@ -0,0 +1,8 @@
+key = input("Please enter the Key : ")
+value = input("Please enter the Value : ")
+
+myDict = {}
+
+# Add Key-Value Pair to a Dictionary in Python
+myDict.update({key:value})
+print("\nUpdated Dictionary = ", myDict)
